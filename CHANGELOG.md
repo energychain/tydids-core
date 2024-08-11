2f2f8e8ff7cb35073e7d3c0d4ad5d9e96942acd0
Thorsten Zoerner <me@thorsten-zoerner.com>
2024-08-12 01:42:17 +0200

    Fix: Doublicate Naming of variables

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,7 @@
+## [1.0.17](https://github.com/energychain/tydids-core/compare/v1.0.16...v1.0.17) (2024-08-11)
+
+### Bug Fixes
+
+* Tweek UI elements to plugin SSI consent wallet as given in https://github.com/energychain/tydids-consent-wallet ([9273bf1](https://github.com/energychain/tydids-core/commit/9273bf13abb913b60cd8e198008648b973977b63))
+
 ## [1.0.16](https://github.com/energychain/tydids-core/compare/v1.0.15...v1.0.16) (2024-08-11)
--- src/TyDIDs.js
+++ src/TyDIDs.js
@@ -11,9 +11,9 @@ window.TyDIDs = {
 if(typeof document !== 'undefined') {
     document.addEventListener('DOMContentLoaded', () => {
         if(typeof $ !== 'undefined') {
-            const ssiObject = document.getElementById('ssiObject');
-            if (ssiObject) {            
-                const ssiObject = JSON.parse(ssiObject.textContent);
+            const ssiElement = document.getElementById('ssiObject');
+            if (ssiElement) {            
+                const ssiObject = JSON.parse(ssiElement.textContent);
                 const ssi = new window.TyDIDs.SSI(ssiObject.privateKey);   
                 // Tweek some UI stuff from SSI html files.
                 $('.card-footer').append(`<button class="btn btn-dark float-end">Add to Wallet</button>`);
